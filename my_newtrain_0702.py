b6c4f3fedffee2baf086e2b9870dd61f6caf914e
oooJiun <lingjiun24@gmail.com>
2024-07-04 16:34:45 +0800

    SRGAN

--- my_newtrain_0702.py
+++ my_newtrain_0702.py
@@ -10,18 +10,14 @@ from glob import glob
 from tqdm import tqdm
 from PIL import Image
 import random
-# from my_ssim import ssim, SSIMLossWithThreshold
+from SRGAN_model import Generator as Generator_clear
   
 class ImageDataset(Dataset):
     def __init__(self, image_dir, mask_dir, clear_dir, blur_dir, transform=None):
-        self.image_paths = sorted(glob(os.path.join(image_dir, '*.bmp')))  # Adjust extension if needed
-        self.mask_paths = sorted(glob(os.path.join(mask_dir, '*.bmp')))    # Adjust extension if needed
+        self.image_paths = sorted(glob(os.path.join(image_dir, '*.bmp')))
+        self.mask_paths = sorted(glob(os.path.join(mask_dir, '*.bmp')))
         self.clear_paths = sorted(glob(os.path.join(clear_dir, '*.bmp'))) 
         self.blur_paths = sorted(glob(os.path.join(blur_dir, '*.bmp'))) 
-        # self.gt_paths = sorted(glob(os.path.join(gt_dir, '*.png')))        # Adjust extension if needed
-        # self.mask2_paths = sorted(glob(os.path.join(mask2_dir, '*.png')))
-        # assert len(self.image_paths) == len(self.mask2_paths), \
-        #     "The number of images, masks, and ground truth images must be the same"
         
         self.transform = transform
 
@@ -36,23 +32,17 @@ class ImageDataset(Dataset):
         mask_path = self.mask_paths[idx]
         clear_path = self.clear_paths[random_idx_c]
         blur_path = self.blur_paths[random_idx_b]
-        # gt_path = self.gt_paths[idx]
-        # mask2_path = self.mask2_paths[idx]
 
         image = cv2.imread(img_path, cv2.COLOR_BGR2RGB)
         mask_img = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
         clear_img = cv2.imread(clear_path, cv2.COLOR_BGR2RGB)
         blur_img = cv2.imread(blur_path, cv2.COLOR_BGR2RGB)
-        # gt_img = cv2.imread(gt_path, cv2.COLOR_BGR2RGB)
-        # mask2_img = cv2.imread(mask2_path, cv2.IMREAD_GRAYSCALE)
 
         if self.transform:
             image = self.transform(image)
             mask_img = self.transform(mask_img)
             clear_img = self.transform(clear_img)
             blur_img = self.transform(blur_img)
-            # gt_img = self.transform(gt_img)
-            # mask2_img = self.transform(mask2_img)
 
         return image, mask_img, clear_img, blur_img
 
@@ -90,6 +80,7 @@ class BaseConv(nn.Module):
         self.conv = nn.Conv2d(in_channels, out_channels, kernel, stride, kernel // 2)
         self.conv.weight.data.normal_(0, 0.01)
         self.conv.bias.data.zero_()
+        self.In = nn.InstanceNorm2d(out_channels)
         self.bn = nn.BatchNorm2d(out_channels)
         self.bn.weight.data.fill_(1)
         self.bn.bias.data.zero_()
@@ -97,7 +88,7 @@ class BaseConv(nn.Module):
     def forward(self, input):
         input = self.conv(input)
         if self.use_bn:
-            input = self.bn(input)
+            input = self.In(input)
         if self.activation:
             input = self.activation(input)
 
@@ -111,13 +102,13 @@ class Generator(nn.Module):
             BaseConv(4, 64, 3, 1, activation=nn.ReLU(inplace=False), use_bn=True),
             BaseConv(64, 128, 3, 1, activation=nn.ReLU(inplace=False), use_bn=True),
             BaseConv(128, 256, 3, 1, activation=nn.ReLU(inplace=False), use_bn=True),
-            BaseConv(256, 256, 1, 1, activation=nn.ReLU(inplace=False), use_bn=True),
-            BaseConv(256, 512, 3, 1, activation=nn.ReLU(inplace=False), use_bn=True),
+            # BaseConv(256, 256, 1, 1, activation=nn.ReLU(inplace=False), use_bn=True),
+            BaseConv(256, 256, 3, 1, activation=nn.ReLU(inplace=False), use_bn=True),
         )
 
         self.decoder = nn.Sequential(
-            BaseConv(512, 256, 3, 1, activation=nn.ReLU(inplace=False), use_bn=True),
-            BaseConv(256, 256, 3, 1, activation=nn.ReLU(inplace=False), use_bn=True),
+            # BaseConv(512, 256, 3, 1, activation=nn.ReLU(inplace=False), use_bn=True),
+            # BaseConv(256, 256, 3, 1, activation=nn.ReLU(inplace=False), use_bn=True),
             BaseConv(256, 128, 3, 1, activation=nn.ReLU(inplace=False), use_bn=True),
             BaseConv(128, 64, 3, 1, activation=nn.ReLU(inplace=False), use_bn=True),
             BaseConv(64, 32, 3, 1, activation=nn.ReLU(inplace=False), use_bn=True),
@@ -125,6 +116,7 @@ class Generator(nn.Module):
         )
 
     def forward(self, real_image, mask):
+    # def forward(self, masked_image):
         input_combined = torch.cat([real_image, mask], dim=1)
         encoder = self.encoder(input_combined)
         # encoder = self.encoder(masked_image)
@@ -132,6 +124,7 @@ class Generator(nn.Module):
         decoder = self.decoder(encoder)
 
         return decoder
+
     
 class Discriminator(nn.Module):
     def __init__(self):
@@ -141,11 +134,11 @@ class Discriminator(nn.Module):
             BaseConv(3, 64, 3, 2, activation=nn.LeakyReLU(0.2,inplace=False), use_bn=False),  # Input: 3x256x256, Output: 64x128x128
             BaseConv(64, 128, 3, 2, activation=nn.LeakyReLU(0.2,inplace=False), use_bn=True),  # Output: 128x64x64
             BaseConv(128, 256, 3, 4, activation=nn.LeakyReLU(0.2,inplace=False), use_bn=True),  # Output: 256x32x32
-            BaseConv(256, 512, 3, 4, activation=nn.LeakyReLU(0.2,inplace=False), use_bn=True),  # Output: 512x16x16
+            BaseConv(256, 256, 3, 4, activation=nn.LeakyReLU(0.2,inplace=False), use_bn=True),  # Output: 512x16x16
             # BaseConv(512, 512, 3, 2, activation=nn.LeakyReLU(0.2,inplace=False), use_bn=True),  # Output: 512x8x8
             # BaseConv(512, 512, 3, 2, activation=nn.LeakyReLU(0.2,inplace=False), use_bn=True),  # Output: 512x4x4
-            BaseConv(512, 512, 3, 1, activation=nn.LeakyReLU(0.2,inplace=False), use_bn=True),  # Output: 512x4x4
-            nn.Conv2d(512, 1, 4, 1, 0),  # Output: 1x1x1 (Real/Fake)
+            # BaseConv(512, 512, 3, 1, activation=nn.LeakyReLU(0.2,inplace=False), use_bn=True),  # Output: 512x4x4
+            nn.Conv2d(256, 1, 4, 1, 0),  # Output: 1x1x1 (Real/Fake)
             nn.Sigmoid()
         )
     
@@ -153,7 +146,7 @@ class Discriminator(nn.Module):
         return self.model(x).view(-1, 1)
     
 def Create_nets():
-    generator_clear = Generator()
+    generator_clear = Generator_clear()
     generator_blur = Generator()
     discriminator_clear = Discriminator()
     discriminator_blur = Discriminator()
@@ -189,8 +182,8 @@ def save_image(images, output_dir, epoch, tag):
             img = Image.fromarray(img)
         img.save(os.path.join(output_dir, f'{i}_{tag}.png'))
 
-def concat_images(file_path):
-    for i in range(2):
+def concat_images(file_path, batch_size):
+    for i in range(batch_size):
         images = [
             Image.open(os.path.join(file_path, str(i)+'_input.png')),
             
@@ -204,7 +197,7 @@ def concat_images(file_path):
             Image.open(os.path.join(file_path, str(i)+'_output_blur2img.png')),
             
             # Image.open(os.path.join(file_path, str(i)+'_gt.png')),
-            Image.open('/home/oscar/Desktop/Focus/gray_image.png'),
+            # Image.open('/home/oscar/Desktop/Focus/gray_image.png'),
             
         ]
 
@@ -252,9 +245,11 @@ def train_model(image_dir, mask_dir, clear_dir, blur_dir, epochs, batch_size, le
     # blur_image_loader = ClearImageDataset(blur_image_dir, transform=transform)
 
     generator_clear, generator_blur, discriminator_clear, discriminator_blur = Create_nets()
-    optimizer_G = optim.Adam(generator_clear.parameters(), lr=learning_rate)
-    optimizer_D_clear = optim.Adam(discriminator_clear.parameters(), lr=learning_rate)
-    optimizer_D_blur = optim.Adam(discriminator_blur.parameters(), lr=learning_rate)
+    optimizer_G = optim.Adam(list(generator_clear.parameters()) + list(generator_blur.parameters()), lr=learning_rate, betas=(0.5, 0.999))
+    optimizer_D_clear = optim.Adam(discriminator_clear.parameters(), lr=learning_rate, betas=(0.5, 0.999))
+    optimizer_D_blur = optim.Adam(discriminator_blur.parameters(), lr=learning_rate, betas=(0.5, 0.999))
+    # optimizer_D_clear = optim.Adam(discriminator_clear.parameters(), lr=learning_rate)
+    # optimizer_D_blur = optim.Adam(discriminator_blur.parameters(), lr=learning_rate)
    
 
     for epoch in range(epochs):
@@ -275,64 +270,63 @@ def train_model(image_dir, mask_dir, clear_dir, blur_dir, epochs, batch_size, le
             img, mask_img, clear_img, blur_img = img.to(device), mask_img.to(device), clear_img.to(device), blur_img.to(device)
 
             optimizer_G.zero_grad()
-            optimizer_D_clear.zero_grad()
-            optimizer_D_blur.zero_grad()
-
             valid = torch.ones((img.size(0), 1)).to(device)
             fake = torch.zeros((img.size(0), 1)).to(device)
+            flag = 0
 
-            output_img2clear = generator_clear(img, mask_img)
-            pred_clear = discriminator_clear(output_img2clear)
-            output_clear2img = generator_blur(output_img2clear, mask_img)
+            output_img2clear = generator_clear(img, 1-mask_img)
+            output_clear2img = generator_blur(output_img2clear, 1-mask_img)
 
-            output_img2blur = generator_blur(img, 1-mask_img)
-            pred_blur = discriminator_blur(output_img2blur)
-            output_blur2img = generator_clear(output_img2blur, 1-mask_img)
-
-            loss_blur2clear = criterion_GAN(pred_clear, valid)
-            loss_clear2blur = criterion_GAN(pred_blur, valid)
+            output_img2blur = generator_blur(img, mask_img)
+            output_blur2img = generator_clear(output_img2blur, mask_img)
 
             loss_cycle_c = cycle_consistency_loss(img, output_clear2img)
             loss_cycle_b = cycle_consistency_loss(img, output_blur2img)
+            if(loss_cycle_b.item() + loss_cycle_c.item() < 0.15):
+                flag = 1
 
+            if(flag and (epoch+1)%1 == 0):
+                pred_clear = discriminator_clear(output_img2clear)
+                pred_blur = discriminator_blur(output_img2blur)
+                loss_blur2clear = criterion_GAN(pred_clear, valid)
+                loss_clear2blur = criterion_GAN(pred_blur, valid)
 
-            loss_G = (loss_blur2clear + loss_clear2blur)*0.1 + (loss_cycle_c + loss_cycle_b)*0.9
+                loss_G = loss_blur2clear + loss_clear2blur + loss_cycle_c + loss_cycle_b
+            else :
+                loss_G = loss_cycle_c + loss_cycle_b
             loss_G.backward(retain_graph=True)
             optimizer_G.step()
 
+            if(flag and (epoch+1)%1 == 0):
+                optimizer_D_clear.zero_grad()
+                optimizer_D_blur.zero_grad()
 
-            # discriminator clear
-            
-            # clear_images = [clear_image_loader.random_image().to(device) for _ in range(img.shape[0])]
-            # clear_stacked_images = torch.stack(clear_images)
+                # discriminator clear
+                pred_real_clear = discriminator_clear(clear_img)
+                loss_real_clear = criterion_GAN(pred_real_clear, valid)
+                loss_fake_clear = criterion_GAN(pred_clear.detach(), fake)
 
-            pred_real_clear = discriminator_clear(clear_img)
-            loss_real_clear = criterion_GAN(pred_real_clear, valid)
-            # loss_fake_clear = criterion_GAN(discriminator_clear(output_img2clear).detach(), fake)
-            loss_fake_clear = criterion_GAN(pred_clear.detach(), fake)
+                loss_D_clear = 0.5 * (loss_real_clear + loss_fake_clear)
 
-            loss_D_clear = 0.5 * (loss_real_clear + loss_fake_clear)
+                loss_D_clear.backward(retain_graph=True)
+                optimizer_D_clear.step()
 
-            loss_D_clear.backward(retain_graph=True)
-            optimizer_D_clear.step()
+                # discriminator blur
+                pred_real_blur = discriminator_blur(blur_img)
+                loss_real_blur = criterion_GAN(pred_real_blur, valid)
+                loss_fake_blur = criterion_GAN(pred_blur.detach(), fake)
 
-            # discriminator blur
-            
-            # blur_images = [blur_image_loader.random_image().to(device) for _ in range(img.shape[0])]
-            # blur_stacked_images = torch.stack(blur_images)
+                loss_D_blur = 0.5 * (loss_real_blur + loss_fake_blur)
 
-            pred_real_blur = discriminator_blur(blur_img)
-            loss_real_blur = criterion_GAN(pred_real_blur, valid)
-            loss_fake_blur = criterion_GAN(pred_blur.detach(), fake)
+                loss_D_blur.backward(retain_graph=True)
+                optimizer_D_blur.step()
 
-            loss_D_blur = 0.5 * (loss_real_blur + loss_fake_blur)
+                epoch_loss_D_clear += loss_D_clear.item()
+                epoch_loss_D_blur += loss_D_blur.item()
 
-            loss_D_blur.backward(retain_graph=True)
-            optimizer_D_blur.step()
 
             epoch_loss_gen += loss_G.item()
-            epoch_loss_D_clear += loss_blur2clear.item()
-            epoch_loss_D_blur += loss_clear2blur.item()
+            
             epoch_loss_cycle_c += loss_cycle_c.item()
             epoch_loss_cycle_b += loss_cycle_b.item()
 
@@ -345,7 +339,7 @@ def train_model(image_dir, mask_dir, clear_dir, blur_dir, epochs, batch_size, le
         save_image(output_clear2img, result_path, epoch, 'output_clear2img')
         save_image(output_blur2img, result_path, epoch, 'output_blur2img')
         # save_image(mask2_img, result_path, epoch, 'mask2')
-        concat_images(result_path)            
+        concat_images(result_path, batch_size)            
 
 
         save_path_G_clear = os.path.join(model_save_path, f'generator_clear.pth')
@@ -363,8 +357,8 @@ def train_model(image_dir, mask_dir, clear_dir, blur_dir, epochs, batch_size, le
         
         print(f"Epoch [{epoch+1}/{epochs}], "
               f"D_clear Loss: {epoch_loss_D_clear:.4f}, D_blur Loss: {epoch_loss_D_blur:.4f}, "
-              f"cycle_loss Clear: {epoch_loss_cycle_c:.4f}, cycle_loss Blur: {epoch_loss_cycle_b:.4f}")
-            #   f"Gen Loss: {epoch_loss_gen:.4f}")
+              f"cycle_loss Clear: {epoch_loss_cycle_c:.4f}, cycle_loss Blur: {epoch_loss_cycle_b:.4f}",
+              f"Gen Loss: {epoch_loss_gen:.4f}")
         
 
 if __name__ == '__main__':
@@ -379,12 +373,12 @@ if __name__ == '__main__':
     parser.add_argument('--mask_dir', type=str, default='/home/oscar/Desktop/dataset/SG_dataset/dataset/train_data/1204gt')
     parser.add_argument('--clear_path', type=str, default='/home/oscar/Desktop/dataset/SG_dataset/dataset/train_data/FCFB/FCC/FC/')
     parser.add_argument('--blur_path', type=str, default='/home/oscar/Desktop/dataset/SG_dataset/dataset/train_data/FCFB/FBB/FB/')
-    parser.add_argument('--mask2_dir', type=str, default='/home/oscar/Desktop/SG/source_map')
-    parser.add_argument('--gt_dir', type=str, default='/home/oscar/Desktop/dataset/DPDD/dd_dp_dataset_png/train_c/target')
+    # parser.add_argument('--mask2_dir', type=str, default='/home/oscar/Desktop/SG/source_map')
+    # parser.add_argument('--gt_dir', type=str, default='/home/oscar/Desktop/dataset/DPDD/dd_dp_dataset_png/train_c/target')
     
     parser.add_argument('--epochs', type=int, default=10000, help='Number of epochs to train.')
     parser.add_argument('--batch_size', type=int, default=2, help='Batch size for training.')
-    parser.add_argument('--learning_rate', type=float, default=0.00005, help='Learning rate for the optimizer.')
+    parser.add_argument('--learning_rate', type=float, default=0.0001, help='Learning rate for the optimizer.')
     parser.add_argument('--model_save_path', type=str, default='./saved_models_cycle', help='Path to save the trained model.')
     parser.add_argument('--result_path', type=str, default='./0709_result/1')
 
